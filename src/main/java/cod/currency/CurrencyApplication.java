ad77f0bfdbfb00a8f4a9cb71f777ea129f4f1e54
João Marques <joaoMAMarques@users.noreply.github.com>
2020-08-01 20:47:01 +0100

    initialize project

---
+++ src/main/java/cod/currency/CurrencyApplication.java
@@ -0,0 +1,13 @@
+package cod.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyApplication.class, args);
+    }
+
+}
