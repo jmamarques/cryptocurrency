731f4a42c29ad1420182a9377b6b94c9064bd2d1
jmamarques <jm.a.marques.93@gmail.com>
2020-11-08 01:32:38 +0000

    add aop add logger aspect with statistics meters
    Signed-off-by: jmamarques <jm.a.marques.93@gmail.com>

--- src/main/java/cod/currency/CurrencyApplication.java
+++ src/main/java/cod/currency/CurrencyApplication.java
@@ -2,10 +2,12 @@ package cod.currency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAspectJAutoProxy
 // @EnableEncryptableProperties no needed
 public class CurrencyApplication {
 
