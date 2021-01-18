c5a0628f28b844c2755292e927a969058b83a434
jmamarques <jm.a.marques.93@gmail.com>
2021-01-18 00:30:05 +0000

    add actuator and metrics
    Signed-off-by: jmamarques <jm.a.marques.93@gmail.com>

--- src/main/java/cod/currency/configuration/aspect/MetricsAspect.java
+++ src/main/java/cod/currency/configuration/aspect/MetricsAspect.java
@@ -2,20 +2,50 @@ package cod.currency.configuration.aspect;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.aspectj.lang.annotation.Before;
+import io.micrometer.core.instrument.Timer;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
-/*@Aspect
-@Component*/
+import java.time.Duration;
+
+@Aspect
+@Component
 public class MetricsAspect {
 
-    private Counter counter;
+    private final Counter counter;
+    private final Counter counterFail;
+    private final Timer timer;
 
     public MetricsAspect(MeterRegistry meterRegistry) {
-        //this.counter = meterRegistry.counter("account.fetch", "type", "fromAspect");
+
+        this.counter = meterRegistry.counter("controllerCounter", "type", "fromAspect");
+        this.counterFail = meterRegistry.counter("controllerCounterFail", "type", "fromAspect");
+        this.timer = meterRegistry.timer("controllerTimer", "type", "fromAspect");
     }
 
-    @Before("execution(* *.*(..))")
-    public void increment() {
+    @Around("execution(* cod.currency.controller..*(..)))")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         counter.increment();
+
+        StopWatch stopWatch = new StopWatch();
+
+        Object proceed = null;
+        stopWatch.start();
+
+        try {
+            proceed = joinPoint.proceed(joinPoint.getArgs());
+        } catch (Throwable throwable) {
+            counterFail.increment();
+            throw throwable;
+        } finally {
+            stopWatch.stop();
+        }
+
+        timer.record(Duration.ofDays(stopWatch.getTotalTimeMillis()));
+
+        return proceed;
     }
 }
--- src/main/java/cod/currency/controller/rest/ParameterController.java
+++ src/main/java/cod/currency/controller/rest/ParameterController.java
@@ -4,12 +4,14 @@ import cod.currency.service.ParameterService;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * JMA - 22/11/2020 10:49
  **/
-@RestController("parameters")
+@RestController
+@RequestMapping("/parameters")
 public class ParameterController {
     private final ParameterService parameterService;
 
